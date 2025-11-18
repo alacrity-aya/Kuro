d235c12dec89f2a847a01424ca3d292b50f431fd
alacrity-aya <horc5555@gmail.com>
2025-11-18 11:07:05 +0800

    using bpf/makefile to build bpf and solve lsp problem

--- bpf/src/tc_process.c
+++ bpf/src/tc_process.c
@@ -3,6 +3,19 @@
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
 
+typedef struct {
+    bool gress; //true-egress false-ingress
+    uint32_t time_scale;
+    uint64_t rate_bps;
+} Rule;
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __type(key, __u32);
+    __type(value, Rule);
+    __uint(max_entries, 1);
+} cgroup_rules SEC(".maps");
+
 char LICENSE[] SEC("license") = "GPL";
 
 SEC("cgroup_skb/ingress")
